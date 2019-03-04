e261ce27bef9b7fc95caf59b68ca5b7836686075
Mats Gron(zezdon) <ed54res@hotmail.com>
2019-03-04 13:20:49 +0100

    Draw board - game.js

--- js/states/Game.js
+++ js/states/Game.js
@@ -18,6 +18,7 @@ Match3.GameState = {
     this.board = new Match3.Board(this, this.NUM_ROWS, this.NUM_COLS, this.NUM_VARIATIONS);
     this.board.consoleLog();
 
+    this.drawBoard();
   },
   createBlock: function(x, y, data) {
     var block = this.blocks.getFirstExists(false);
@@ -31,6 +32,29 @@ Match3.GameState = {
     }
 
     return block;
+  },
+  drawBoard: function() {
+    var i, j, block, square, x, y, data;
+
+    //semi-transparent black squares
+    var squareBitmap = this.add.bitmapData(this.BLOCK_SIZE + 4, this.BLOCK_SIZE + 4);
+    squareBitmap.ctx.fillStyle = '#000';
+    squareBitmap.ctx.fillRect(0, 0, this.BLOCK_SIZE + 4, this.BLOCK_SIZE + 4);
+
+    for(i = 0; i < this.NUM_ROWS; i++) {
+      for(j = 0; j < this.NUM_COLS; j++) {
+        x = 36 + j * (this.BLOCK_SIZE + 6);
+        y = 150 + i * (this.BLOCK_SIZE + 6);
+
+        square = this.add.sprite(x, y, squareBitmap);
+        square.anchor.setTo(0.5);
+        square.alpha = 0.2;
+
+        this.createBlock(x, y, {asset: 'block' + this.board.grid[i][j], row: i, col: j});
+      }
+    }
+
+    this.game.world.bringToTop(this.blocks);
   }
 
 
